017c3965be7352717664258b13962a911d04ebd0
caliisack755 <caliisack755@gmail.com>
2026-06-30 04:28:56 +0000

    week 6

---
+++ tutorial_4/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Lecturer dr = new Lecturer("barca5", "ALi", "OOP", "Faculty of Information Technology");
+        dr.displayInfo();
+        dr.displaySubject();
+        dr.displayDepartment();
+    }
+}
